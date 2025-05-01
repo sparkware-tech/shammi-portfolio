8ff8e1186e7c40272849c52acf1766cfaaf10a67
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2025-05-01 20:54:33 +0530

    make portfolio popup

---
+++ assets/constants/portfolio.js
@@ -0,0 +1,44 @@
+const portfolioItems = [
+  {
+    id: "app-1",
+    category: "app",
+    title: "App 1",
+    description: "Lorem ipsum, dolor sit amet consectetur.",
+    image: "assets/img/portfolio/app-1.jpg",
+  },
+  {
+    id: "app-2",
+    category: "app",
+    title: "App 2",
+    description: "Second app, fast and modern.",
+    image: "assets/img/portfolio/app-2.jpg",
+  },
+  {
+    id: "web-1",
+    category: "web",
+    title: "Web Project 1",
+    description: "A slick website design.",
+    image: "assets/img/portfolio/books-1.jpg",
+  },
+  {
+    id: "web-1",
+    category: "product",
+    title: "Web Project 1",
+    description: "A slick website design.",
+    image: "assets/img/portfolio/books-1.jpg",
+  },
+  {
+    id: "web-1",
+    category: "branding",
+    title: "Web Project 1",
+    description: "A slick website design.",
+    image: "assets/img/portfolio/books-1.jpg",
+  },
+  {
+    id: "web-1",
+    category: "product",
+    title: "Web Project 1",
+    description: "A slick website design.",
+    image: "assets/img/portfolio/books-1.jpg",
+  },
+];
