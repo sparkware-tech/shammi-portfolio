6e2642f183753bf334e6900af992443fa6bf92de
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2025-05-01 22:16:27 +0530

    modify portfolio popup

--- assets/constants/portfolio.js
+++ assets/constants/portfolio.js
@@ -2,8 +2,8 @@ const portfolioItems = [
   {
     id: "1",
     category: "web",
-    title: "App 1",
-    description: "A slick website design.",
+    title: "LEO club Website",
+    description: "Explore the vibrant Leo Club of District 306 C2 at the University of Sri Jayawardenepura.",
     image: "assets/img/portfolio/books-1.jpg",
   },
   {
